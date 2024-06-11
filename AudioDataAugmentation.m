1f171598681206a3c2a37b2a277525b78aedd104
robertabattaglia <r.battaglia8@studenti.unisa.it>
2024-06-11 17:16:52 +0200

    Update and rename ultimo.m to AudioDataAugmentation.m

--- AudioDataAugmentation.m
+++ AudioDataAugmentation.m
@@ -17,11 +17,11 @@ file_rumore_vento = 'vento.wav';
 [rumore_pioggia, fs_rumore] = audioread(file_rumore_pioggia);
 [rumore_vento, fs_rumore_vento] = audioread(file_rumore_vento);
 
-% Parametro di miscelazione
+%Parametro di miscelazione
 alpha = 0.4;
 
 for i = 1:length(file_audio_originali)
-    % Percorso completo del file audio originale
+    %Percorso completo del file audio originale
     percorso_audio_originale = fullfile(file_audio_originali(i).folder, file_audio_originali(i).name);
 %file_audio_originali(i): Accede al i-esimo file nella lista dei file trovati.
 %file_audio_originali(i).folder: Ottiene il percorso della cartella che contiene il file corrente.
@@ -33,7 +33,8 @@ for i = 1:length(file_audio_originali)
 
 % Assicuro che le frequenze di campionamento siano uguali
 if fs ~= fs_rumore
-   error('La frequenza di campionamento dei due file audio deve essere la stessa.');
+    % Ricampionamento del rumore alla stessa frequenza di campionamento del file audio originale
+    rumore_pioggia = resample(rumore_pioggia, fs, fs_rumore);
 end
 
 % Se le durate non sono uguali, faccio in modo che abbiano la stessa durata
@@ -52,16 +53,27 @@ lunghezza_minima = min(length(rumore_pioggia), lunghezza_originale);
 rumore_troncato = rumore_pioggia(1:lunghezza_minima);
 audio_originale_troncato = audio_originale(1:lunghezza_minima);
 
-%Riduzione iterativa del rumore per evitare il clipping
+%Evitare il clipping
+%Massima ampiezza del segnale audio originale
+max_originale = max(abs(audio_originale_troncato));
+
+%Massima ampiezza del rumore
+max_rumore = max(abs(rumore_troncato));
+
+% Calcolo del nuovo alpha
+alpha_star = round((1 - max_originale) / max_rumore, 2);
+
+if alpha_star < alpha
+    alpha = alpha_star;
+end
+
+% Calcolo il segnale audio risultante utilizzando il nuovo alpha
 augmented_audio = audio_originale_troncato + (alpha * rumore_troncato);
-while any(abs(augmented_audio) > 1)
-     alpha = alpha - 0.01;
-     augmented_audio = audio_originale_troncato + (alpha * rumore_troncato);
- end
 
   
 %Percorso per salvare il nuovo file audio
-percorso_augmented_audio = fullfile('new_dataset', ['augmented_' file_audio_originali(i).name]);
+tipo_rumore = 'pioggia';
+percorso_augmented_audio = fullfile('new_dataset', sprintf('augmented_%s_alpha%.2f_%s', tipo_rumore, alpha, file_audio_originali(i).name));
 
 %Salvo il nuovo file audio
 audiowrite(percorso_augmented_audio, augmented_audio, fs,'BitsPerSample',32);
@@ -81,15 +93,26 @@ lunghezza_minima_vento = min(length(rumore_vento), lunghezza_originale);
 rumore_troncato_vento = rumore_vento(1:lunghezza_minima_vento);
 audio_originale_vento = audio_originale(1:lunghezza_minima_vento);
 
-%Riduzione iterativa del rumore per evitare il clipping
-alpha = 0.4;  % Reset alpha per il rumore del vento
-augmented_audio_vento = audio_originale_vento + (alpha * rumore_troncato_vento);
-while any(abs(augmented_audio_vento) > 1)
-    alpha = alpha - 0.01;
-    augmented_audio_vento = audio_originale_vento + (alpha * rumore_troncato_vento);
+%Evitare il clipping
+% Massima ampiezza del segnale audio originale
+max_originale = max(abs(audio_originale_vento));
+
+%Massima ampiezza del rumore
+max_rumore = max(abs(rumore_troncato_vento));
+
+% Calcolo del nuovo alpha 
+alpha = 0.4;
+alpha_star = round((1 - max_originale) / max_rumore, 2);
+
+if alpha_star < alpha
+    alpha = alpha_star;
 end
 
-percorso_augmented_audio_vento = fullfile('new_dataset', ['augmented_vento_' file_audio_originali(i).name]);
+% Calcolo il segnale audio risultante utilizzando il nuovo alpha
+augmented_audio_vento = audio_originale_vento + (alpha * rumore_troncato_vento);
+
+tipo_rumore = 'vento';
+percorso_augmented_audio_vento = fullfile('new_dataset', sprintf('augmented_%s_alpha%.2f_%s', tipo_rumore, alpha, file_audio_originali(i).name));
 
 audiowrite(percorso_augmented_audio_vento, augmented_audio_vento, fs,'BitsPerSample',32);
 
